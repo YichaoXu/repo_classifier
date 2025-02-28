072193b33983cf11d84322e7b88269fbe6562d8a
Yichao Xu <yxu166@jh.edu>
2025-02-27 23:18:17 -0500

    chore: Update project configuration and package metadata
    - Renamed package from "repo-classifier" to "repo_classifier"
    - Updated package name in pyproject.toml and setup.py
    - Added project URLs in pyproject.toml for homepage, documentation, and source code

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setup(
-    name="repo-classifier",
+    name="repo_classifier",
     version="0.1.0",
     author="YichaoXU",
     author_email="yxu166@jhu.edu",
